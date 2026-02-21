2fdc895f8a44215d63291b60a3cbae93c0c7d1c4
Toby Payne <75158953+tpguy825@users.noreply.github.com>
2026-02-21 02:22:12 +0000

    ...very stupid

--- fetch-specific.sh
+++ fetch-specific.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #echo Enter commit id: 
 #read cid
+cid=$1
 git clone https://github.com/tpguy825/webtiles-archives webtiles-archives-$cid
 cd webtiles-archives-$cid
 git reset --hard $cid
